d6736576211d276383406a415ca4ab66cb64c71b
Vaishnav2607 <vaishnavnarigiri267@gmail.com>
2024-04-02 23:42:30 +0530

    Add chat sessions functionality

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import streamlit as st
 from llm_chains import load_normal_chain
-# from langchain.memory import StreamlitChatMessagesHistory
-
+import os
 from langchain_community.chat_message_histories import (
     StreamlitChatMessageHistory,
 )
+import yaml
+# from utils import save_chat_history_json
+# from langchain.memory import StreamlitChatMessagesHistory
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+def save_chat_history_json(chat_history, file_path):
+    with open(file_path, "w") as f:
+        json_data = [message.dict() for message in chat_history]
+        json.dump(json_data, f)
 
 def load_chain(chat_history):
     return load_normal_chain(chat_history)
@@ -17,14 +26,20 @@ def set_send_input():
     st.session_state.send_input = True
     clear_input_field()
 
+def save_chat_history():
+    if(st.session.state.history!=[]):
+        save_chat_history_json(st.session_state.history, config["chat_history_path"] + "random" + ".json")
 def main():
     st.title("Multimodal chat app")
     chat_container = st.container()
     st.sidebar.title("Chat Sessions")
+    chat_sessions = ["new_session"] + os.listdir(config["chat_history_path"])
 
     if "send_input" not in st.session_state:
         st.session_state.send_input = False
         st.session_state.user_question = ""
+    
+    st.sidebar.selectbox("Select a chat Session", chat_sessions, key="session_key")
 
     chat_history = StreamlitChatMessageHistory(key="history")
     llm_chain = load_chain(chat_history = chat_history)  # Pass chat_history to load_chain
@@ -46,6 +61,8 @@ def main():
             st.write("Chat History: ")
             for message in chat_history.messages:
                 st.chat_message(message.type).write(message.content)
+    print(chat_history.messages[0].dict())
+    save_chat_history()
 print("test")
  
 
